7375ab651e0144f258928b8bec3e26c1e1d59e0b
pangyan wong <pangyanwong@pangyans-Air.local>
2024-02-03 17:48:38 +0800

    CLS-167 - live scoreboard - initial version

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import classes from './app.module.scss'
+import Footer from "./components/Footer/index.jsx"
+import Header from "./components/Header/index.jsx"
+import ScoreboardsGrid from "./components/Header/index.jsx"
+
+function App() {
+  return (
+    <div className={classes.app}>
+        <Header/>
+        <main>
+            <ScoreboardsGrid/>
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
